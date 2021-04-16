b900443e074dad81529e10c08f2c4f64567f9403
Administrator <Administrator@172.31.1.201>
2021-04-16 22:42:18 +0000

    Updated checksum Credit CArd validation logic

--- CreditCardsAPI/src/main/java/com/cc/utils/ProjectUtils.java
+++ CreditCardsAPI/src/main/java/com/cc/utils/ProjectUtils.java
@@ -3,15 +3,34 @@
  */
 package com.cc.utils;
 
+import java.util.stream.Stream;
+
 /**
  * @author Administrator
  *
  */
 public class ProjectUtils {
 	public static boolean validateCardNumber(String number) {
-		if(number.startsWith("4"))
-			return false;
-		else 
-			return true;
+		int sum = 0;
+		int length = number.length();
+		// convert string to in array
+		int[] nums = Stream.of(number.split("")).mapToInt(Integer::parseInt).toArray();
+
+		
+		for(int i=0;i<nums.length;i++) {
+			// get digits in reverse
+			int num = nums[length - i -1];
+			
+			//multiply with 2 on every other number
+			
+			if(i%2 == 1)
+				num = num *2;
+			// if more than 9 round it back to 0
+			sum += num > 9? num-9:num;
+		}
+		
+		//validate checksum
+		return sum % 10 == 0;
+		
 	}
 }
