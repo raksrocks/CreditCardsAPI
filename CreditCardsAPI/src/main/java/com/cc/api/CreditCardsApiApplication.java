80d8c77063158f7917f00df88cd69d96256c580b
Administrator <Administrator@172.31.1.201>
2021-04-16 15:59:48 +0000

    App Created using STS

---
+++ CreditCardsAPI/src/main/java/com/cc/api/CreditCardsApiApplication.java
@@ -0,0 +1,14 @@
+package com.cc.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardsApiApplication.class, args);
+		System.out.println("Verify");
+	}
+
+}
---
+++ CreditCardsAPI/src/test/java/com/cc/api/CreditCardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
