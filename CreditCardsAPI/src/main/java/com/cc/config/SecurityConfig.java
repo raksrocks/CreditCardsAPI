6011ac83af1f95eab109e5d79adb41d6abcba007
Rakesh <rakeshsingarapu@gmail.com>
2021-04-21 10:57:00 +0000

    Added Security layer - Basic authentication

---
+++ CreditCardsAPI/src/main/java/com/cc/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.cc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Administrator
+ *
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+		 http
+         .csrf().disable()
+         .authorizeRequests().anyRequest().authenticated()
+         .and()
+         .httpBasic();
+    }
+  
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+    	auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}password")
+            .roles("USER");
+    }
+}
